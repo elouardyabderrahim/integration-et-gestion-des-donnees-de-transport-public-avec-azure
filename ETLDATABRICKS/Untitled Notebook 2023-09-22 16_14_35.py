a82caa49051625c8598e857af31892c1b91b0ff0
elouardyabderrahim <101024060+elouardyabderrahim@users.noreply.github.com>
2023-09-22 16:25:46 +0100

    add the link script with row derectory

--- ETLDATABRICKS/Untitled Notebook 2023-09-22 16_14_35.py
+++ ETLDATABRICKS/Untitled Notebook 2023-09-22 16_14_35.py
@@ -1,2 +1,15 @@
 # Databricks notebook source
-print("hello world!!")
+import pandas as pd
+import random
+from datetime import datetime, timedelta
+from pyspark.sql import SparkSession
+spark.conf.set(
+    f"fs.azure.account.key.bentalebstorageacc.dfs.core.windows.net", 
+    "f7yngxT+I8Wzy+J0GAoTGBtjqo92greqVvmofq1zHx8msMTSl33Kws93ICPi6fIAR2z5XFn4t/wD+AStcTUhOQ=="
+)
+raw = "abfss://publictransportdata@bentalebstorageacc.dfs.core.windows.net/raw/"
+
+df = spark.read.format("csv").option("inferSchema", "True").option("header",
+"True").option("delimeter",",").load(raw)
+
+display(df)
