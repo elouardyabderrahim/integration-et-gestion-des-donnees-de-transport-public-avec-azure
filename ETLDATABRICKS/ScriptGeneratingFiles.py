38d84392b1b7538e8320df7dc91c597e5a1acaff
elouardyabderrahim <elouardyabderrahim>
2023-09-27 10:13:50 +0000

    etl and generating file scripts

--- ETLDATABRICKS/ScriptGeneratingFiles.py
+++ ETLDATABRICKS/ScriptGeneratingFiles.py
@@ -1,66 +1,113 @@
 # Databricks notebook source
-'''import pandas as pd
+# Databricks notebook source
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+import pandas as pd
 import random
 from datetime import datetime, timedelta
+from pyspark.sql import SparkSession
+import config
 
-# Generate data for January 2023
-start_date = datetime(2023, 1, 1)
-end_date = datetime(2023, 1, 31)
-date_generated = [start_date + timedelta(days=x) for x in range(0, (end_date-start_date).days)]
+
+# COMMAND ----------
+
+# Mounting data lake
+storageAccountName = config.STORAGE_ACCOUNT_NAME
+
+storageAccountAccessKey = config.STORAGE_ACCOUNT_KEY
+sasToken = config.SAS_TOKEN
+blobContainerName = "publictransportdata"
+mountPoint = "/mnt/publictransportdata/"
+if not any(mount.mountPoint == mountPoint for mount in dbutils.fs.mounts()):
+  try:
+    dbutils.fs.mount(
+      source = "wasbs://{}@{}.blob.core.windows.net".format(blobContainerName, storageAccountName),
+      mount_point = mountPoint,
+      extra_configs = {'fs.azure.sas.' + blobContainerName + '.' + storageAccountName + '.blob.core.windows.net': sasToken}
+    )
+    print("mount succeeded!")
+  except Exception as e:
+    print("mount exception", e)
+
+# COMMAND ----------
+
+start_year = 2021
+end_year = 2024
 
 transport_types = ["Bus", "Train", "Tram", "Metro"]
 routes = ["Route_" + str(i) for i in range(1, 11)]
 stations = ["Station_" + str(i) for i in range(1, 21)]
 
-# Randomly select 5 days as extreme weather days
-extreme_weather_days = random.sample(date_generated, 5)
+for year in range(start_year, end_year):
+    for month in range(1, 13):  
+        # Generate data for the current month
+        start_date = datetime(year, month, 1)
+        next_month = month + 1 if month < 12 else 12
+        day = 1 if month < 12 else 31
+        end_date = start_date.replace(day=day, month=next_month) - timedelta(days=1)
+        date_generated = [start_date + timedelta(days=x) for x in range(0, (end_date - start_date).days + 1)]
 
-data = []
 
-for date in date_generated:
-    for _ in range(32):  # 32 records per day to get a total of 992 records for January
-        transport = random.choice(transport_types)
-        route = random.choice(routes)
+        # Randomly select 5 days as extreme weather days
+        extreme_weather_days = random.sample(date_generated, 5)
 
-        # Normal operating hours
-        departure_hour = random.randint(5, 22)
-        departure_minute = random.randint(0, 59)
+        data = []
 
-        # Introducing Unusual Operating Hours for buses
-        if transport == "Bus" and random.random() < 0.05:  # 5% chance
-            departure_hour = 3
-
-        departure_time = f"{departure_hour:02}:{departure_minute:02}"
-
-        # Normal duration
-        duration = random.randint(10, 120)
-
-        # Introducing Short Turnarounds
-        if random.random() < 0.05:  # 5% chance
-            duration = random.randint(1, 5)
-
-        # General delay
-        delay = random.randint(0, 15)
-
-        # Weather Impact
-        if date in extreme_weather_days:
-            # Increase delay by 10 to 60 minutes
-            delay += random.randint(10, 60)
-
-            # 10% chance to change the route
-            if random.random() < 0.10:
+        for date in date_generated:
+            for _ in range(32):  # 32 records per day to get a total of 992 records for each month
+                transport = random.choice(transport_types)
                 route = random.choice(routes)
 
-        total_minutes = departure_minute + duration + delay
-        arrival_hour = departure_hour + total_minutes // 60
-        arrival_minute = total_minutes % 60
-        arrival_time = f"{arrival_hour:02}:{arrival_minute:02}"
+                # Normal operating hours
+                departure_hour = random.randint(5, 22)
+                departure_minute = random.randint(0, 59)
 
-        passengers = random.randint(1, 100)
-        departure_station = random.choice(stations)
-        arrival_station = random.choice(stations)
+                # Introducing Unusual Operating Hours for buses
+                if transport == "Bus" and random.random() < 0.05:  # 5% chance
+                    departure_hour = 3
 
-        data.append([date, transport, route, departure_time, arrival_time, passengers, departure_station, arrival_station, delay])
+                departure_time = f"{departure_hour:02}:{departure_minute:02}"
 
-df = pd.DataFrame(data, columns=["Date", "TransportType", "Route", "DepartureTime", "ArrivalTime", "Passengers", "DepartureStation", "ArrivalStation", "Delay"])
-'df.to_csv("public_transport_data.csv", index=False)'''
+                # Normal duration
+                duration = random.randint(10, 120)
+
+                # Introducing Short Turnarounds
+                if random.random() < 0.05:  # 5% chance
+                    duration = random.randint(1, 5)
+
+                # General delay
+                delay = random.randint(0, 15)
+
+                # Weather Impact
+                if date in extreme_weather_days:
+                    # Increase delay by 10 to 60 minutes
+                    delay += random.randint(10, 60)
+
+                    # 10% chance to change the route
+                    if random.random() < 0.10:
+                        route = random.choice(routes)
+
+                total_minutes = departure_minute + duration + delay
+                arrival_hour = departure_hour + total_minutes // 60
+                arrival_minute = total_minutes % 60
+                arrival_time = f"{arrival_hour:02}:{arrival_minute:02}"
+
+                passengers = random.randint(1, 100)
+                departure_station = random.choice(stations)
+                arrival_station = random.choice(stations)
+
+                data.append([date, transport, route, departure_time, arrival_time, passengers, departure_station, arrival_station, delay])
+
+        # Create a DataFrame for the current month's data
+        month_i = str(year) + "_" + str(month).zfill(2)
+        df = pd.DataFrame(data, columns=["Date", "TransportType", "Route", "DepartureTime", "ArrivalTime", "Passengers", "DepartureStation", "ArrivalStation", "Delay"])
+        df.to_csv("/dbfs/mnt/publictransportdata/raw/rawTransportDataOf_"+ month_i + ".csv" , index=False)
+
+    print(f"Generated data for year - " + str(year))
+
+
+# COMMAND ----------
+
+dbutils.fs.unmount("/mnt/publictransportdata/")
---
+++ ETLDATABRICKS/dataIntegration.py
@@ -0,0 +1,110 @@
+# Databricks notebook source
+import pandas as pd
+from datetime import datetime
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import StringType,IntegerType
+from pyspark.sql.functions import to_date, year, month, day, hour, minute, when, avg, regexp_replace, mean, count, round
+from pyspark.sql import SparkSession
+import config
+
+
+# COMMAND ----------
+
+# Mounting data lake
+storageAccountName = config.STORAGE_ACCOUNT_NAME
+storageAccountAccessKey = config.STORAGE_ACCOUNT_KEY
+sasToken = config.SAS_TOKEN
+
+blobContainerName = "publictransportdata"
+mountPoint = "/mnt/publictransportdata/"
+if not any(mount.mountPoint == mountPoint for mount in dbutils.fs.mounts()):
+  try:
+    dbutils.fs.mount(
+      source = "wasbs://{}@{}.blob.core.windows.net".format(blobContainerName, storageAccountName),
+      mount_point = mountPoint,
+      extra_configs = {'fs.azure.sas.' + blobContainerName + '.' + storageAccountName + '.blob.core.windows.net': sasToken}
+    )
+    print("mount succeeded!")
+  except Exception as e:
+    print("mount exception", e)
+
+# COMMAND ----------
+
+raw = f"{mountPoint}raw/"
+processed = f"{mountPoint}processed/"
+info = "No file has Been processed !"
+processed_count = 0
+raw_files = dbutils.fs.ls(raw)
+raw_csv_files = [f.path for f in raw_files if f.name.endswith(".csv")]
+raw_file_count = len(raw_csv_files)
+processed_files = dbutils.fs.ls(processed + "transport/")
+processed_csv_files = [f.path for f in processed_files if f.name.endswith(".csv")]
+for i in range(raw_file_count):
+    specific_name = "dbfs:" + processed + "transport/"+  raw_csv_files[i].replace("raw", "").split("/")[-1].split(".")[0] + "_processed.csv"
+    if processed_count == 2:
+        break
+    elif specific_name in processed_csv_files:
+        continue
+    else:
+        if processed_count == 0:
+            info = "Processed file : \n"
+        df = spark.read.format("csv").option("inferSchema", "True").option("header",
+        "True").option("delimeter",",").load(raw_csv_files[i])
+
+        # year month day
+        df = df.withColumn("Year", year(df.Date))
+        df = df.withColumn("Month",month(df.Date))
+        df = df.withColumn("Day", day(df.Date))
+
+        # Change Date column to datetime
+        df = df.withColumn("Date", to_date(df["Date"]))
+
+        # Add DelayCategory column
+        df = df.withColumn("DelayCategory",
+            when(df["Delay"] <= 0, "On Time")
+            .when(df["Delay"] <= 10, "Short Delay")
+            .when(df["Delay"] <= 20, "Medium Delay")
+            .otherwise("Long Delay")
+        )
+
+        # Add Duration column
+        df = df.withColumn("ArrivalTime", regexp_replace(df["ArrivalTime"], "24:", "00:"))
+        df = df.withColumn("ArrivalTime", regexp_replace(df["ArrivalTime"], "25:", "00:"))
+        df = df.withColumn("Duration", (hour(df["ArrivalTime"]) * 60 + minute(df["ArrivalTime"])) - (hour(df["DepartureTime"]) * 60 + minute(df["DepartureTime"])))
+        df = df.withColumn("Duration", when(df["Duration"] < 0, df["Duration"] + 12*60).otherwise(df["Duration"]))
+
+        pandasDF = df.toPandas()
+        file_name = f"/dbfs/mnt/publictransportdata/processed/transport/" + raw_csv_files[i].replace("raw", "").split("/")[-1].split(".")[0] + "_processed.csv"
+        pandasDF.to_csv(file_name, index=False)
+
+        # Peak hours
+        df_peak_hours = df.withColumn("DepartureHour", hour(df["ArrivalTime"]))
+        df_peak_hours = df_peak_hours.groupBy("DepartureHour").agg(
+            round(mean("Passengers"), 2).alias("AvgPassengers"))
+        # Identify peak and off-peak times based on passenger numbers
+        avg_passengers = df.select(avg(df["Passengers"])).collect()[0][0]
+        df_peak_hours = df_peak_hours.withColumn("Peak_hour", df_peak_hours["AvgPassengers"] >= avg_passengers)
+
+        pandasDF = df_peak_hours.toPandas()
+        file_name = f"/dbfs/mnt/publictransportdata/processed/peakhour/peakhour" + raw_csv_files[i].replace("raw", "").split("/")[-1].split(".")[0] + "_processed.csv"
+        pandasDF.to_csv(file_name, index=False)
+
+        # routes analysis
+        df_avg = df.groupBy("Route").agg(
+            round(mean("Passengers"), 2).alias("AvgPassengers"),
+            round(mean("Delay"), 2).alias("AvgDelay"),
+            round(count("Route"), 2).alias("Trips")
+        )
+
+        pandasDF = df_avg.toPandas()
+        file_name = f"/dbfs/mnt/publictransportdata/processed/routes/routes" + raw_csv_files[i].replace("raw", "").split("/")[-1].split(".")[0] + "_processed.csv"
+        pandasDF.to_csv(file_name, index=False)
+        
+        processed_count += 1
+        print("processed Month - " + str(i + 1))
+        info += "- " + raw_csv_files[i].split("/")[-1].split(".")[0] + "\n"
+
+# COMMAND ----------
+
+dbutils.fs.unmount("/mnt/publictransportdata/")
+print(info)
